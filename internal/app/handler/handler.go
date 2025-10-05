b912b909b8b1a401e2f02420a76b859b318d1638
Marina Aleshechkina <68868094+roboticlongcat@users.noreply.github.com>
2025-10-05 20:20:39 +0300

    классы по теме

--- internal/app/handler/handler.go
+++ internal/app/handler/handler.go
@@ -23,26 +23,26 @@ func (h *Handler) GetPatients(ctx *gin.Context) {
 	var patients []repository.Patient
 	var err error
 
-	searchQuery := ctx.Query("query")
-	if searchQuery == "" {
+	patientSearchQuery := ctx.Query("query")
+	if patientSearchQuery == "" {
 		patients, err = h.Repository.GetPatients()
 		if err != nil {
 			logrus.Error(err)
 		}
 	} else {
-		patients, err = h.Repository.GetPatientsByName(searchQuery)
+		patients, err = h.Repository.GetPatientsByName(patientSearchQuery)
 		if err != nil {
 			logrus.Error(err)
 		}
 	}
 
-	count, _ := h.Repository.GetCalculationItemsCount(1)
+	patients_count, _ := h.Repository.GetCalculationItemsCount(1)
 
 	ctx.HTML(http.StatusOK, "index.html", gin.H{
-		"patients":      patients,
-		"query":         searchQuery,
-		"count":         count,
-		"calculationID": 1,
+		"patients":             patients,
+		"query":                patientSearchQuery,
+		"patients_count":       patients_count,
+		"insulinCalculationID": 1,
 	})
 }
 
@@ -63,24 +63,34 @@ func (h *Handler) GetPatient(ctx *gin.Context) {
 	})
 }
 
-func (h *Handler) GetCalculation(ctx *gin.Context) {
+func (h *Handler) GetInsulinCalculation(ctx *gin.Context) {
 	IDStr := ctx.Param("id")
 	calculationID, err := strconv.Atoi(IDStr)
 
 	if err != nil {
-		logrus.Error("Неверный ID расчета:", err)
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Неверный ID расчета"})
+		logrus.Error("Неверный ID расчета инсулина:", err)
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Неверный ID расчета инсулина"})
 		return
 	}
 
-	var patients []repository.Patient
-
-	patients, err = h.Repository.GetCalculation(calculationID)
+	insulinCalculation, err := h.Repository.GetInsulinCalculation(calculationID)
 	if err != nil {
 		logrus.Error(err)
 	}
 
-	ctx.HTML(http.StatusOK, "calculation.html", gin.H{
+	var patients []repository.Patient
+	var IDPatients [5]int = [5]int{1, 5}
+
+	for _, IDpatient := range insulinCalculation.Items {
+		for _, number := range IDPatients {
+			if number == IDpatient.PatientID {
+				patient, _ := h.Repository.GetPatient(number)
+				patients = append(patients, patient)
+			}
+		}
+	}
+
+	ctx.HTML(http.StatusOK, "insulin_calculation.html", gin.H{
 		"patients": patients,
 	})
 }
