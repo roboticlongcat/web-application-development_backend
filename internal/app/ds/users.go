bd1abf85e47b5940757cc0c35a68863bcfa4fdcb
Marina Aleshechkina <68868094+roboticlongcat@users.noreply.github.com>
2025-10-05 20:45:54 +0300

    Update users.go

--- internal/app/ds/users.go
+++ internal/app/ds/users.go
@@ -1,7 +1,7 @@
 package ds
 
 type User struct {
-	ID           int    `gorm:"primaryKey"`
+	User_ID      uint   `gorm:"primaryKey"`
 	Username     string `gorm:"type:varchar(150);not null;unique"`
 	PasswordHash string `gorm:"type:varchar(255);not null"`
 	IsModerator  bool   `gorm:"type:boolean;not null;default:false"`
