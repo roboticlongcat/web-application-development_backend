7895b60a569b27c1a15ad1bf9f10b1c7e37b5e4c
Marina Aleshechkina <68868094+roboticlongcat@users.noreply.github.com>
2025-10-05 20:46:38 +0300

    Update handler.go

--- internal/app/handler/handler.go
+++ internal/app/handler/handler.go
@@ -17,23 +17,20 @@ func NewHandler(r *repository.Repository) *Handler {
 	}
 }
 
-// RegisterHandler Функция, в которой мы отдельно регистрируем маршруты, чтобы не писать все в одном месте
 func (h *Handler) RegisterHandler(router *gin.Engine) {
 	router.GET("/", h.GetPatients)
 	router.GET("/patient/:id", h.GetPatient)
-	router.GET("/calculation/:id", h.GetCalculation)
-	router.POST("/calculation/add", h.AddPatientToCalculation)
-	router.POST("/calculation/delete", h.DeleteCalculation)
+	router.GET("/insulin_calculation/:id", h.GetInsulinCalculation)
+	router.POST("/insulin_calculation/add", h.AddPatientToInsulinCalculation)
+	router.POST("/insulin_calculation/delete", h.DeleteInsulinCalculation)
 }
 
-// RegisterStatic То же самое, что и с маршрутами, регистрируем статику
 func (h *Handler) RegisterStatic(router *gin.Engine) {
 	router.LoadHTMLGlob("templates/*")
 	router.Static("static/styles", "./resources/styles")
 	router.Static("/img", "./resources/img")
 }
 
-// errorHandler для более удобного вывода ошибок
 func (h *Handler) errorHandler(ctx *gin.Context, errorStatusCode int, err error) {
 	logrus.Error(err.Error())
 	ctx.JSON(errorStatusCode, gin.H{
